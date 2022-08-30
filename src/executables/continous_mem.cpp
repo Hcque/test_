842c9df1caf41defe73ebb777159d7682de78506
Hcque <hc995639@gmail.com>
2022-08-30 15:11:51 +0800

    B

---
+++ src/executables/continous_mem.cpp
@@ -0,0 +1,126 @@
+
+#include <cstdint>
+#include <cassert>
+#include <mutex>
+#include <queue>
+#include <vector>
+
+
+#define SZ 32
+
+class RecordBufferSegment
+{
+  public:
+    char *Reserve(const uint32_t size)
+    {
+        auto result = content_ + size_;
+        size_ += size;
+        return result;
+    }
+
+    RecordBufferSegment *Reset()
+    {
+        size_ = 0;
+        return this;
+    }
+
+//   private:
+    char content_[SZ];
+    uint32_t size_;
+};
+
+template <typename T, typename Allocator>
+class ObjectPool
+{
+  public:
+    T* Get()
+    {
+        T *t;
+        if (reuse_que_.size() == 0) 
+        {
+            t = alloc_.New();
+            curr_size_ ++ ;
+            if (curr_size_ > set_limit_) assert(0);
+        }
+        else
+        {
+            t = reuse_que_.front(); reuse_que_.pop();
+            alloc_.Reuse(t);
+        }
+        return t;
+
+    }
+
+    void Release()
+    {
+
+    }
+
+  private:
+    std::mutex mutex_;
+    std::queue<T *> reuse_que_;
+    int set_limit_, reuse_limit_;
+    int curr_size_;
+
+    Allocator alloc_;
+
+};
+
+class Version
+{
+
+};
+
+class RecordBufferSegmentAllocator
+{
+    void *New() { return new RecordBufferSegment(); }
+    void Delete(RecordBufferSegment *ptr) { delete ptr; }
+    void Reuse(RecordBufferSegment *ptr) { ptr->Reset(); }
+};
+
+class UndoBuffer
+{
+  public:
+    class Iterator
+    {
+      public:
+      Iterator(std::vector<RecordBufferSegment *>::iterator curr_segment,
+            int offset) 
+            : curr_segment_(curr_segment)
+            , offset_(offset) {}
+
+        friend class UndoBuffer;
+
+        Version &operator*() const
+        {
+            return *(reinterpret_cast<Version *> ( (*curr_segment_)->content_ + offset_ ) );
+        }
+        Version *operator->() const
+        {
+            return reinterpret_cast<Version *> ( (*curr_segment_)->content_ + offset_ );
+        }
+
+      private:
+        std::vector<RecordBufferSegment*>::iterator curr_segment_;
+        uint32_t offset_;
+
+    };
+
+    Iterator begin() { return {buffers_.begin(), 0}; }
+    Iterator end() { return { buffers_.end(), 0}; }
+  private:
+    std::vector<RecordBufferSegment *> buffers_;
+
+};
+
+
+
+
+int main()
+{
+    UndoBuffer *b = new UndoBuffer();
+
+    return 0;
+
+
+}
--- src/executables/linked_list.cpp
+++ src/executables/linked_list.cpp
@@ -1,5 +1,11 @@
 
 // lock contention , basic mode
+// summary : stomic data structre key is 
+// 1)define lod
+// 2) do the operations in while(1) loop
+// 3) CaS in the end to jump the loop ( success) 
+// 
+
 
 #include <iostream>
 #include <thread>
@@ -8,78 +14,152 @@
 #include <atomic>
 #include <cassert>
 
-struct Node
+
+using TxnID = uint64_t;
+
+struct VersionContent
 {
-    Node() {}
-    Node(int v) : value(v), next(nullptr) {}
-    int value;
-    // std::atomic<Node *> next;
-    Node* next;
-    // std::mutex lk;
+    std::atomic<uint64_t> txn_id, read_ts, begin, end;
+};
+
+
+struct Version
+{
+    int val;
+    std::atomic<uint64_t> txn_id, read_ts, begin, end;
+    std::atomic<Version *> next;
+
+    static Version *InitRecord()
+    {
+        return new Version();
+    }
+
+    void AtomicRead(TxnID cur_txn_id)
+    {
+        while (1) 
+        {
+            if (read_ts.load() < cur_txn_id) read_ts.store( cur_txn_id );
+            uint64_t read_latch = 0; // old
+            if (txn_id.compare_exchange_strong(read_latch, 0))
+            {
+                return;
+            }
+        }
+    }
+
+    // void AtomicWrite(uint64_t cur_txn_id)
+    // {
+    //     // allocate
+    //     Version *version = new Version();
+    //     version->txn_id.store( cur_txn_id ); 
+    //     // cur_txn_id is old
+    //     while (1)
+    //     {
+    //         if (txn_id.load() > 0 && cur_txn_id > read_ts.load())
+    //         {
+    //             // set latch of both
+    //             txn_id.store(cur_txn_id);
+
+    //             if (txn_id.compare_exchange_strong(cur_txn_id, cur_txn_id) )
+    //             {
+    //                 return;
+    //             }
+
+    //         }
+    //     }
+
+    // }
+
+
 };
 
 struct List
 {
-    std::atomic<Node *> head;
-    // Node* head;
-    // std::mutex mutex_;
+    std::atomic<Version *> chain;
+    int inplace_for_update{0};
+    std::atomic<uint64_t> txn_id, read_ts, begin, end;
+
+    // problem : branch
+    void AtomicRead(uint64_t txn_id)
+    {
+        if (txn_id < begin.load())
+        {
+            // chase the version chain
+            Version *cur = chain.load();
+            while (cur)
+            {
+                if (cur->end.load() > txn_id ) break;
+                cur = cur->next.load();
+            }
+
+            // read the cur
+        }
+        else
+        {
+            // read the current
+        }
+    }
+
+
+    void AtomicWrite(uint64_t cur_txn_id)
+    {
+        // allocate
+        Version *version = new Version();
+        version->txn_id.store( cur_txn_id ); 
+        // cur_txn_id is old
+        while (1)
+        {
+            if (txn_id.load() > 0 && cur_txn_id > read_ts.load())
+            {
+                // set latch of both
+                txn_id.store(cur_txn_id);
+
+                if (txn_id.compare_exchange_strong(cur_txn_id, cur_txn_id) )
+                {
+                    return;
+                }
+
+            }
+        }
+
+    }
+
 };
 
-// void insert(int value)
+
 void insert(List* list, int value)
 {
     // std::cout << value << std::endl;
     //  list->mutex_.lock();
-    Node* n = new Node;
-    n->value = value;
 
+    Version* version = Version::InitRecord();
+    // std::cout << version << std::endl;
     while (1)
     {
+        // pre-store for atomic check
+        Version *old = list->chain.load();
 
-    Node* prev = list->head.load();
-    // prev->lk.lock();
-    // auto cur = prev->next;
-    // cur->lk.lock();
-    // list->mutex_.unlock();
+        // copy head content
+        version->next.store(list->chain.load());
+        version->val = list->inplace_for_update;
 
-    // while (cur)
-    // {
-        // if (cur->value > value) 
-        // {
-            // break;
-        // }
-        // prev = cur;
-        // cur = cur->next;
-    // }
+        // inplace-update
+        list->inplace_for_update = value;
 
-    n->next = prev;
-    // prev->next = n;
-    if  ( list->head.compare_exchange_strong(prev, n ) ) return ;
+        // list->chain.store(version);
+        if  ( list->chain.compare_exchange_strong(old, version ))
+        {
+            return;
+        }
     }
-
-
-    // cur->lk.unlock();
-    // prev->lk.unlock();
-    // list->mutex_.unlock();
-}
-
-
-
-// std::thread each(insert, &l, )
-
-void delete_(List *list, int value)
-{
-
 }
 
 std::vector<std::thread> ths;
 
-
 int main(int argc, char *argv[])
 {
     List *l = new List();
-    l->head.store(new Node(-1));
-    // l->head->value = -1;
+    l->chain.store(0);
 
     // std::this_thread::sleep_for( std::chrono::seconds(2) );
 
@@ -92,19 +172,17 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < NUM; i ++ ) ths[i].join();
     std::cout << "DONE === \n";
+    // std::cout << C.load() << std::endl;
 
-        // insert(&l, i);
-    Node *cur = l->head;
-    int cnt = -1;
+    Version *cur = l->chain.load();
+    int cnt = 0;
     while (cur)
     {
         // std::cout << cur->value << std::endl;
-        // cval = cur->value;
-        // assert(cval -1 == prev);
-        // prev = cval;
         cnt ++ ;
+    // std::cout << "cnt : " << cur << std::endl;
 
-        cur = cur->next;
+        cur = cur->next.load();
     }
     std::cout << "cnt : " << cnt << std::endl;
 
